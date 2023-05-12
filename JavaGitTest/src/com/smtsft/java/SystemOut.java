f5652e67ea090b59886b52a29bcfd88f8ac4e6b3
ykkim <ykkim7451@gmail.com>
2023-05-12 14:27:26 +0900

    real frist person commit try

--- JavaGitTest/src/com/smtsft/java/SystemOut.java
+++ JavaGitTest/src/com/smtsft/java/SystemOut.java
@@ -4,9 +4,9 @@ public class SystemOut {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String[] names = {"홍길동", "이영순"};
+		String[] names = {"홍길동", "이영순", "이영환"};
 		for (int i=0; i < names.length; i++) {
-			System.out.format("Hello, %s!\n", names[i]);
+			System.out.format("Hello, %s!\n", names[i]);			
 		}
 	}
 }
