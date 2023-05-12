db1b11938935adfac4f17188635c2f2ee07a8241
ykkim <ykkim7451@gmail.com>
2023-05-12 14:08:22 +0900

    first commit

---
+++ JavaGitTest/src/com/smtsft/java/SystemOut.java
@@ -0,0 +1,12 @@
+package com.smtsft.java;
+
+public class SystemOut {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] names = {"홍길동", "이영순"};
+		for (int i=0; i < names.length; i++) {
+			System.out.format("Hello, %s!\n", names[i]);
+		}
+	}
+}
