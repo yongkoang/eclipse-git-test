08888af8bf0ac74cf6bb24c30323fa6db875cd9d
ykkim <ykkim7451@gmail.com>
2023-05-12 14:24:25 +0900

    first person second commit

--- JavaGitTest/src/com/smtsft/java/SystemOut.java
+++ JavaGitTest/src/com/smtsft/java/SystemOut.java
@@ -5,8 +5,8 @@ public class SystemOut {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String[] names = {"홍길동", "이영순", "한철수"};
-		for (int i=0; i < names.length; i++) {
-			System.out.format("Hello, %s!\n", names[i]);
+		for (String name : names) {
+			System.out.format("Hello, %s!\n", name);
 		}
 	}
 }
