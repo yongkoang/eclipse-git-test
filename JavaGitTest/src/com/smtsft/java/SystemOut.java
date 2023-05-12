8c3190ba63bd326e113ee8cd575103ede45c18a5
ykkim <ykkim7451@gmail.com>
2023-05-12 14:21:16 +0900

    modify string array

--- JavaGitTest/src/com/smtsft/java/SystemOut.java
+++ JavaGitTest/src/com/smtsft/java/SystemOut.java
@@ -4,7 +4,7 @@ public class SystemOut {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String[] names = {"홍길동", "이영순"};
+		String[] names = {"홍길동", "이영순", "한철수"};
 		for (int i=0; i < names.length; i++) {
 			System.out.format("Hello, %s!\n", names[i]);
 		}
